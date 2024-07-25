43224a20dc4ec16fabe639dbe37371f6e3cc8fca
arajol <164122642+arajol@users.noreply.github.com>
2024-07-25 11:04:44 +0600

    first commit

---
+++ randomjs.js
@@ -0,0 +1,37 @@
+// let x = Math.floor((Math.random() * 9999));
+// console.log(parseInt(x))
+a = "Ihaveyou"
+b = a.split("")
+console.log(b)
+// function name(num1) {
+//     total = num1
+//     return total
+// }
+
+// console.log(name(5))
+
+// function name(num1) {
+//     let numButton = document.getElementById(num1)
+//     numButton.addEventListener("click", function(){
+//         btnValue = numButton.innerHTML
+//         document.getElementById("button-click-input").value = btnValue
+//     })
+// }
+
+// name("btn-1")
+
+// biscuitPrice = 1234
+
+// if (biscuitPrice == 1234) {
+//     console.log("match")
+// } else {
+//     console.log("no match")
+// }
+
+// let generatedPin = 1234
+// let inputNumber = 1234
+// if (generatedPin == inputNumber) {
+//     console.log("match")
+// } else {
+//     console.log("no match")
+// }
